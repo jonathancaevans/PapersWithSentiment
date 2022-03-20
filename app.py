ab37cd0014bd03f0b849a6d54ec9eb2edf3b7eab
Jonathan <jonathan.ca.evans@gmail.com>
2022-03-20 03:26:01 -0400

    Bunch of changes lol

---
+++ app.py
@@ -0,0 +1,82 @@
+#Flask imports
+from flask import Flask, request, session, render_template, jsonify
+from flask_cors import CORS, cross_origin
+
+#scihub api import from local directory
+from scihub import *
+
+#Utils imports
+import PyPDF2
+import io
+import os
+
+#Google cloud imports
+from google.cloud import language_v1
+from google.cloud import vision_v1
+
+#Set up flask app and CORS
+app = Flask(__name__, static_url_path="", static_folder="build", template_folder="build")
+
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+#Set up credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "googleCloudKey.json"
+
+@app.route("/")
+def index():
+    """Main Page"""
+    return render_template("index.html")
+
+@app.route('/semantify', methods=["POST"])
+def semantify():
+	sh = SciHub()
+
+	try:
+		url = request.json.get("url")
+
+		result = sh.fetch(url)
+		p = io.BytesIO(result['pdf']);
+
+		client = vision_v1.ImageAnnotatorClient()
+		mime_type = "application/pdf"
+
+		input_config = {"mime_type": mime_type, "content": p.read()}
+
+		features = [{"type_": vision_v1.Feature.Type.DOCUMENT_TEXT_DETECTION}]
+
+		pages = [1, 2, 3, 4, 5]
+
+		requests = [{"input_config": input_config, "features": features, "pages": pages}]
+
+		response = client.batch_annotate_files(requests=requests)
+
+		text_content=''
+		for image_response in response.responses[0].responses:
+			text_content+=image_response.full_text_annotation.text
+
+		text_content = text_content.replace('\n',' ')
+
+		client = language_v1.LanguageServiceClient()
+
+		type_ = language_v1.Document.Type.PLAIN_TEXT
+		document = {"content": text_content, "type_": type_}
+		encoding_type = None
+
+		response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+
+		sentenceSorted = sorted(response.sentences, key=lambda e: abs(e.sentiment.magnitude),reverse=True)
+
+		sentences = []
+		for sentence in sentenceSorted:
+			sentences.append({"text":sentence.text.content,"score":round(sentence.sentiment.score, 2),"magnitude":round(sentence.sentiment.magnitude, 2)})
+
+	except KeyError:
+		return jsonify({"Error": "Can't find pdf for this paper"})
+	except:
+		return jsonify({"Error": "Misc error"})
+
+	return jsonify({"document":{"score":round(response.document_sentiment.score, 2),"magnitude":round(response.document_sentiment.magnitude, 2)}, "sentences": sentences[:15]})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useRef, useEffect, useState, Fragment} from 'react';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
 
 function App() {
+
+  const [url, setUrl] = useState(null);
+
+  const [document, setDocument] = useState(null);
+  const [sentences, setSentences] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  function handleURLChange(e) {
+  	setUrl(e.target.value);
+  }
+
+  const semantify = async(e) => {
+  	if(url == null || url == ''){
+		return
+	}
+
+	setLoading(true);
+
+	const res = await fetch('/semantify', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			},
+		body: JSON.stringify({'url': url})
+	});
+
+	const body = await res.json();
+
+	setLoading(false);
+
+	if(body['Error'] != null){
+		toast.warn(body['Error'])
+	}
+	else{
+                setDocument(body['document'])
+		setSentences(body['sentences']);
+	}
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToastContainer position="top-right" autoClose={1400} closeOnClick={false} draggable={false} pauseOnHover/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
       </header>
+
+      <div class="container mt-2">
+        <div class="row">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Papers with Sentiment</h5>
+              <p class="card-text">Papers with Sentiment uses Google's NLP sentiment analysis API to output the sentiment of scientific articles. Any scientific paper in PDF format available through Sci-Hub can be analyzed just by pasting in the link to the paper paywall! A positive or more positive score represents positive emotion while a negative score represents negative emotion. Angry and sad are examples of negative emotions while happy and glad would be examples of positive emotion. Magnitude represents how much emotional content is present.</p>
+
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Paper URL" onChange={handleURLChange}/>
+                <button class="btn btn-outline-info" type="button" onClick={semantify} disabled={loading}>Semantify!</button>
+              </div>
+            </div>
+            <ul class="list-group list-group-flush">
+              {loading === false && document != null && <li class="list-group-item">
+                <div>
+                  <h6 class="fa">Overall Document</h6>
+                </div>
+                <span class="badge bg-success">Score: {document["score"]}</span>
+                <span class="badge bg-success">magnitude: {document["magnitude"]}</span>
+              </li>}
+
+              {loading ?
+              <li class="list-group-item placeholder-glow">Loading...</li>
+              :
+              sentences.map(function(sentence, i){
+		if(sentence["score"] > 0){
+                  return <li class="list-group-item">
+                           <div>
+                             <p class="fa">{sentence["text"]}</p>
+                           </div>
+                           <span class="badge bg-success">Score: {sentence["score"]}</span>
+                           <span class="badge bg-secondary">Magnitude: {sentence["magnitude"]}</span>
+                         </li>;
+                }
+                else{
+                  return <li class="list-group-item">
+                           <div>
+                             <p class="fa">{sentence["text"]}</p>
+                           </div>
+                           <span class="badge bg-danger">Score: {sentence["score"]}</span>
+                           <span class="badge bg-secondary">Magnitude: {sentence["magnitude"]}</span>
+                         </li>;
+		}
+              }
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
